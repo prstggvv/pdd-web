9bd748b938591688f7938597aebd1f43461e8d7a
Ivan <vany4golos@yandex.ru>
2026-02-24 17:13:13 +0300

    fix: photo

--- src/components/MainComponents/Directions/model/directionsData.ts
+++ src/components/MainComponents/Directions/model/directionsData.ts
@@ -1,7 +1,14 @@
 import LighterImage from '../../../../shared/assets/images/products/lighter.jpg';
-import PlasticaImage from '../../../../shared/assets/images/products/plastica.jpg';
-import SignsImage from '../../../../shared/assets/images/products/signs.jpg';
 import MarkingsImage from '../../../../shared/assets/images/products/markings.jpg';
+
+import SimferopolImage from '../../../../shared/assets/images/projects/simferopol.webp';
+import SimferopolHoverImage from '../../../../shared/assets/images/projects/simferopolwork.png';
+import YaltaImage from '../../../../shared/assets/images/projects/yalta.webp';
+import YaltaHoverImage from '../../../../shared/assets/images/projects/yaltawork.png';
+import PheodosiaImage from '../../../../shared/assets/images/projects/pheodosia.jpg';
+import PheodosiaHoverImage from '../../../../shared/assets/images/projects/pheodosiawork.png';
+import KerchImage from '../../../../shared/assets/images/projects/kerch.webp';
+import KerchHoverImage from '../../../../shared/assets/images/projects/kerchwork.png';
 
 interface IDirectionsDataProps {
   id: string;
@@ -16,28 +23,28 @@ export const directionsData: IDirectionsDataProps[] = [
     id: 'boxing',
     title: 'г. Симферополь',
     subtitle: 'Разработка постоянной схемы Устройство примыкания парковки к автомобильной дороге общего пользования местного значения',
-    defaultImage: SignsImage,
-    hoverImage: PlasticaImage,
+    defaultImage: SimferopolImage,
+    hoverImage: SimferopolHoverImage,
   },
   {
     id: 'kickboxing',
-    title: 'г. Симферополь',
-    subtitle: 'Разработка постоянной схемы Устройство примыкания парковки к автомобильной дороге общего пользования местного значения',
-    defaultImage: PlasticaImage,
-    hoverImage: SignsImage,
+    title: 'г. Ялта',
+    subtitle: 'Разработка проектной схемы ОДД торгового центра',
+    defaultImage: YaltaImage,
+    hoverImage: YaltaHoverImage,
   },
   {
     id: 'mma',
-    title: 'г. Симферополь',
-    subtitle: 'Разработка постоянной схемы Устройство примыкания парковки к автомобильной дороге общего пользования местного значения',
-    defaultImage: MarkingsImage,
-    hoverImage: LighterImage,
+    title: 'г. Феодосия',
+    subtitle: 'Строительство и эксплуатация автозаправочной станции", в полосе отвода автомобильной дороги',
+    defaultImage: PheodosiaImage,
+    hoverImage: PheodosiaHoverImage,
   },
   {
     id: 'bjj',
-    title: 'г. Симферополь',
-    subtitle: 'Разработка постоянной схемы Устройство примыкания парковки к автомобильной дороге общего пользования местного значения',
-    defaultImage: LighterImage,
-    hoverImage: MarkingsImage,
+    title: 'г. Керчь',
+    subtitle: 'Разработка временной схемы на период производства строительно-монтажных работ',
+    defaultImage: KerchImage,
+    hoverImage: KerchHoverImage,
   }
 ];
